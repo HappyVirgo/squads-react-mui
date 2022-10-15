72b41737023cdfd4144e9829dd475920a2c511e3
great-dev1 <twentytwentyaugust@outlook.com>
2022-10-15 17:05:01 -0300

    Add assets/icons

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,4 @@
+export { default as LogoIcon } from './LogoIcon'
+export { default as LogoMobile } from './LogoMobile'
+export { default as DiscordIcon } from './DiscordIcon'
+export { default as SquadsIcon } from './SquadsIcon'
