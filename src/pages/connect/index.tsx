ebe1da1ec6598a688c436b2eac2d67cab36671dc
great-dev1 <twentytwentyaugust@outlook.com>
2022-10-16 14:31:00 -0300

    Add the connect page

--- src/pages/connect/index.tsx
+++ src/pages/connect/index.tsx
@@ -1,10 +1,24 @@
 import { FC } from 'react'
 import Layout from '../../layout'
+import ConnectButton from '../../components/connect-button'
+import CardIcon from '../../assets/images/card-icon.svg'
 
 const Connect: FC = () => {
   return (
     <Layout>
-      <h1>This is a Connect page.</h1>
+      <div className='squad-container'>
+        <h2 className='primary-title'>Your Squads</h2>
+
+        <div className='squad-content d-flex flex-column justify-content-center align-items-center'>
+          <div className='icon-box'>
+            <img src={CardIcon} width={48} height={48} alt='wallet' />
+          </div>
+          <p>
+            Connect your wallet to see which Squads you are in
+          </p>
+          <ConnectButton />
+        </div>
+      </div>
     </Layout>
   )
 }
